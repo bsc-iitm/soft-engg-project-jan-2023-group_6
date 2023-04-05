afe1a9d7aab756531c488def810f0324c0d46441
Anubhav Lohani <anubhavlohani4@gmail.com>
2023-04-05 19:25:22 +0530

    Added user model

--- source/models.py
+++ source/models.py
@@ -2,6 +2,16 @@ from database import db
 
 
 
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(), nullable=False, unique=False)
+    username = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False, unique=False)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    admin = db.Column(db.Boolean(), unique=False, default=False)
+
+
 class Faq(db.Model):
     __tablename__ = 'faq'
     id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
